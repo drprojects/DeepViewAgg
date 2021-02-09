fbba13bbbd75d70ac8f14fbec9b5569b21722cf7
drprojects <dr91@wanadoo.fr>
2021-02-09 13:28:52 +0100

    adding support for optional normalization type and weight standardization in 2D convs. In particular, the setup GroupNorm + Weight Standardization is expected to help 2D models training behave better, since same-setting-images batches have small sizes.

--- torch_points3d/modules/multimodal/modalities/image.py
+++ torch_points3d/modules/multimodal/modalities/image.py
@@ -6,6 +6,61 @@ from torch_points3d.utils.config import *
 from torch_points3d.core.common_modules import Seq, Identity
 
 
+def standardize_weights(weight):
+    weight_mean = weight.mean(dim=1, keepdim=True).mean(dim=2,
+        keepdim=True).mean(dim=3, keepdim=True)
+    weight = weight - weight_mean
+    std = weight.view(weight.size(0), -1).std(dim=1).view(-1, 1, 1, 1) + 1e-5
+    weight = weight / std.expand_as(weight)
+    return weight
+
+
+class Conv2dWS(nn.Conv2d, ABC):
+    """Convd2 with weight standardization.
+
+    Sources:
+        - https://github.com/joe-siyuan-qiao/WeightStandardization
+        - https://pytorch.org/docs/stable/_modules/torch/nn/modules/conv.html
+    """
+
+    def __init__(self, in_channels, out_channels, kernel_size, stride=1,
+                 padding=0, dilation=1, groups=1, bias=True,
+                 padding_mode='zeros'):
+        super(Conv2dWS, self).__init__(in_channels, out_channels, kernel_size,
+            stride=stride, padding=padding, dilation=dilation, groups=groups,
+            bias=bias, padding_mode=padding_mode)
+
+    def forward(self, x):
+        return self._conv_forward(x, standardize_weights(self.weight))
+
+
+class ConvTranspose2dWS(nn.ConvTranspose2d, ABC):
+    """Convd2 with weight standardization.
+
+    source: https://github.com/joe-siyuan-qiao/WeightStandardization
+    """
+
+    def __init__(self, in_channels, out_channels, kernel_size, stride=1,
+                 padding=0, output_padding=0, dilation=1, groups=1, bias=True,
+                 padding_mode='zeros'):
+        super(ConvTranspose2dWS, self).__init__(in_channels, out_channels,
+            kernel_size, stride=stride, padding=padding,
+            output_padding=output_padding, dilation=dilation, groups=groups,
+            bias=bias, padding_mode=padding_mode)
+
+    def forward(self, x, output_size=None):
+        if self.padding_mode != 'zeros':
+            raise ValueError('Only `zeros` padding mode is supported for '
+                             'ConvTranspose2d')
+
+        output_padding = self._output_padding(x, output_size,
+            self.stride, self.padding, self.kernel_size)
+
+        return torch.nn.functional.conv_transpose2d(
+            x, standardize_weights(self.weight), self.bias, self.stride,
+            self.padding, output_padding, self.groups, self.dilation)
+
+
 class ResBlock(nn.Module, ABC):
     """
     Basic ResNet type block
@@ -22,31 +77,41 @@ class ResBlock(nn.Module, ABC):
 
     # TODO: extend to EquiConv https://github.com/palver7/EquiConvPytorch
 
-    def __init__(self, input_nc, output_nc, convolution):
-        if convolution is nn.ConvTranspose2d:
+    def __init__(self, input_nc, output_nc, convolution, normalization):
+        if convolution in [nn.ConvTranspose2d, ConvTranspose2dWS]:
             padding_mode = 'zeros'
         else:
             padding_mode = 'reflect'
 
         super().__init__()
         self.block = (
-            Seq()
-            .append(convolution(input_nc, output_nc, kernel_size=3, stride=1,
-                                padding=1, padding_mode=padding_mode))
-            .append(nn.BatchNorm2d(output_nc))
+            Seq().append(convolution(
+                input_nc,
+                output_nc,
+                kernel_size=3,
+                stride=1,
+                padding=1,
+                padding_mode=padding_mode))
+            .append(normalization(output_nc))
             .append(nn.ReLU())
-            .append(convolution(output_nc, output_nc, kernel_size=3, stride=1,
-                                padding=1, padding_mode=padding_mode))
-            .append(nn.BatchNorm2d(output_nc))
-            .append(nn.ReLU())
-        )
+            .append(convolution(
+                output_nc,
+                output_nc,
+                kernel_size=3,
+                stride=1,
+                padding=1,
+                padding_mode=padding_mode))
+            .append(normalization(output_nc))
+            .append(nn.ReLU()))
 
         if input_nc != output_nc:
             self.downsample = (
-                Seq().append(
-                    nn.Conv2d(input_nc, output_nc, kernel_size=1, stride=1)
-                ).append(nn.BatchNorm2d(output_nc))
-            )
+                Seq().append(nn.Conv2d(
+                    input_nc,
+                    output_nc,
+                    kernel_size=1,
+                    stride=1))
+                .append(normalization(output_nc)))
         else:
             self.downsample = None
 
@@ -64,37 +129,47 @@ class BottleneckBlock(nn.Module, ABC):
     Bottleneck block with residual
     """
 
-    def __init__(self, input_nc, output_nc, convolution, reduction=4, **kwargs):
+    def __init__(self, input_nc, output_nc, convolution, normalization,
+                 reduction=4, **kwargs):
         super().__init__()
 
-        if convolution is nn.ConvTranspose2d:
+        if convolution in [nn.ConvTranspose2d, ConvTranspose2dWS]:
             padding_mode = 'zeros'
         else:
             padding_mode = 'reflect'
 
         self.block = (
-            Seq()
-            .append(nn.Conv2d(input_nc, output_nc // reduction, kernel_size=1,
-                              stride=1))
-            .append(nn.BatchNorm2d(output_nc // reduction))
+            Seq().append(nn.Conv2d(
+                input_nc,
+                output_nc // reduction,
+                kernel_size=1,
+                stride=1))
+            .append(normalization(output_nc // reduction))
             .append(nn.ReLU())
-            .append(convolution(output_nc // reduction, output_nc // reduction,
-                                kernel_size=3, stride=1, padding=1,
-                                padding_mode=padding_mode))
-            .append(nn.BatchNorm2d(output_nc // reduction))
+            .append(convolution(
+                output_nc // reduction,
+                output_nc // reduction,
+                kernel_size=3,
+                stride=1,
+                padding=1,
+                padding_mode=padding_mode))
+            .append(normalization(output_nc // reduction))
             .append(nn.ReLU())
-            .append(nn.Conv2d(output_nc // reduction, output_nc,
-                              kernel_size=1,))
-            .append(nn.BatchNorm2d(output_nc))
-            .append(nn.ReLU())
-        )
+            .append(nn.Conv2d(
+                output_nc // reduction,
+                output_nc,
+                kernel_size=1,))
+            .append(normalization(output_nc))
+            .append(nn.ReLU()))
 
         if input_nc != output_nc:
             self.downsample = (
-                Seq().append(
-                    convolution(input_nc, output_nc, kernel_size=1, stride=1)
-                ).append(nn.BatchNorm2d(output_nc))
-            )
+                Seq().append(convolution(
+                    input_nc,
+                    output_nc,
+                    kernel_size=1,
+                    stride=1))
+                .append(normalization(output_nc)))
         else:
             self.downsample = None
 
@@ -107,7 +182,7 @@ class BottleneckBlock(nn.Module, ABC):
         return out
 
 
-_res_blocks = sys.modules[__name__]
+_local_modules = sys.modules[__name__]
 
 
 class ResNetDown(nn.Module, ABC):
@@ -122,38 +197,48 @@ class ResNetDown(nn.Module, ABC):
     CONVOLUTION = "Conv2d"
 
     def __init__(
-        self, down_conv_nn=[], kernel_size=2, dilation=1, stride=2, N=1,
-            padding=0, block="ResBlock", padding_mode='reflect', **kwargs,
-    ):
-        block = getattr(_res_blocks, block)
+            self, down_conv_nn=[], kernel_size=2, dilation=1, stride=2, N=1,
+            padding=0, block="ResBlock", padding_mode='reflect',
+            normalization='BatchNorm2d', weight_standardization=False,
+            **kwargs):
+        block = getattr(_local_modules, block)
         super().__init__()
         if stride > 1:
             conv1_output = down_conv_nn[0]
         else:
             conv1_output = down_conv_nn[1]
 
-        conv = getattr(nn, self.CONVOLUTION)
+        # Recover the convolution module
+        if weight_standardization:
+            conv = getattr(_local_modules, self.CONVOLUTION + 'WS')
+        else:
+            conv = getattr(nn, self.CONVOLUTION)
+
+        # Recover the normalization module from torch.nn, for GroupNorm
+        # the number of groups is set to distribute ~16 channels per
+        # group: https://arxiv.org/pdf/1803.08494.pdf
+        if normalization == 'GroupNorm':
+            norm = lambda nc: nn.GroupNorm(nc // 16, nc)
+        else:
+            norm = getattr(nn, normalization)
+
         self.conv_in = (
-            Seq()
-            .append(
-                conv(
-                    in_channels=down_conv_nn[0],
-                    out_channels=conv1_output,
-                    kernel_size=kernel_size,
-                    stride=stride,
-                    dilation=dilation,
-                    padding=padding,
-                    padding_mode=padding_mode,
-                )
-            )
-            .append(nn.BatchNorm2d(conv1_output))
-            .append(nn.ReLU())
-        )
+            Seq().append(conv(
+                in_channels=down_conv_nn[0],
+                out_channels=conv1_output,
+                kernel_size=kernel_size,
+                stride=stride,
+                dilation=dilation,
+                padding=padding,
+                padding_mode=padding_mode))
+            .append(norm(conv1_output))
+            .append(nn.ReLU()))
 
         if N > 0:
             self.blocks = Seq()
             for _ in range(N):
-                self.blocks.append(block(conv1_output, down_conv_nn[1], conv))
+                self.blocks.append(block(conv1_output, down_conv_nn[1], conv,
+                     norm))
                 conv1_output = down_conv_nn[1]
         else:
             self.blocks = None
@@ -173,12 +258,13 @@ class ResNetUp(ResNetDown, ABC):
     CONVOLUTION = "ConvTranspose2d"
 
     def __init__(self, up_conv_nn=[], kernel_size=2, dilation=1, stride=2, N=1,
-                 padding=0, padding_mode='zeros', **kwargs):
+                 padding=0, padding_mode='zeros', normalization='BatchNorm2d',
+                 weight_standardization=False, **kwargs):
         super().__init__(
             down_conv_nn=up_conv_nn, kernel_size=kernel_size,
             dilation=dilation, stride=stride, N=N, padding=padding,
-            padding_mode=padding_mode, **kwargs,
-        )
+            padding_mode=padding_mode, normalization=normalization,
+            weight_standardization=weight_standardization, **kwargs)
 
     def forward(self, x, skip):
         if skip is not None:
@@ -189,12 +275,28 @@ class ResNetUp(ResNetDown, ABC):
 class UnaryConv(nn.Module, ABC):
     """1x1 convolution on image."""
 
-    def __init__(self, input_nc, output_nc, norm=None, activation=None):
+    def __init__(self, input_nc, output_nc, normalization=None, activation=None,
+                 weight_standardization=False):
         super().__init__()
-        self.norm = getattr(nn, norm) if norm is not None else norm
+        # Recover the normalization module from torch.nn, for GroupNorm
+        # the number of groups is set to distribute ~16 channels per
+        # group: https://arxiv.org/pdf/1803.08494.pdf
+        if normalization is None:
+            self.norm = None
+        elif normalization == 'GroupNorm':
+            self.norm = lambda nc: nn.GroupNorm(nc // 16, nc)
+        else:
+            self.norm = getattr(nn, normalization)
+
+        # Recover the activation module from torch.nn
         self.activation = getattr(nn, activation) if activation is not None \
             else activation
-        self.conv = nn.Conv2d(input_nc, output_nc, stride=1, kernel_size=1)
+
+        # Build the 1x1 convolution
+        if weight_standardization:
+            self.conv = Conv2dWS(input_nc, output_nc, stride=1, kernel_size=1)
+        else:
+            self.conv = nn.Conv2d(input_nc, output_nc, stride=1, kernel_size=1)
 
     def forward(self, x):
         x = self.conv(x)
