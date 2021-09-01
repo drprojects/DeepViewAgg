fixing bugs for MapImages computation on GPU. The resulting changes should make things faster and allow to preprocess large-scale clouds at once like S3DIS 2cm with 1024x512, in reasonable times.

torch_points3d/core/data_transform/multimodal/image.py
@@ -219,10 +219,10 @@ class MapImages(ImageTransform):
         t_stack_pixels = 0
         t_append = 0
 
-        # Project each image and gather the point-pixel mappings
+        # Project each image and gather the point-pixel mappings (on
+        # CPU or GPU)
         print("    MapImages...")
         for i_image, image in tq(enumerate(images)):
-            print(f'    {torch.cuda.memory_allocated(0)}')
             # Subsample the surrounding point cloud
             torch.cuda.synchronize()
             start = time()
@@ -255,27 +255,32 @@ class MapImages(ImageTransform):
             # provides us with indices of visible points along with
             # corresponding pixel coordinates, depth and mapping
             # features.
+            # (on CPU or GPU)
+            torch.cuda.synchronize()
             start = time()
             out_vm = visi(
                 xyz_to_img, img_opk, img_mask=mask, linearity=linearity,
                 planarity=planarity, scattering=scattering, normals=normals,
                 depth_map_path=depth_map_path)
+            del xyz_to_img, img_opk, linearity, planarity, scattering, normals, mask
 
             # Skip image if no mapping was found
             if out_vm['idx'].shape[0] == 0:
                 continue
 
             # Recover point indices, pixel coordinates and features
+            # (on CPU or GPU)
             point_ids_ = data_sample[self.key].to(device)[out_vm['idx']]
             pix_x_ = out_vm['x'].long()
             pix_y_ = out_vm['y'].long()
             features_ = out_vm['features'].float()
-            del out_vm
+            del out_vm, data_sample
             torch.cuda.synchronize()
             t_visibility += time() - start
 
             # Convert to SameSettingImageData coordinate system with
             # corresponding cropping and resizing
+            # (on CPU or GPU)
             # TODO: add circular padding here if need be
             start = time()
             pix_x_ = pix_x_ // image.proj_upscale
@@ -297,6 +302,7 @@ class MapImages(ImageTransform):
 
             # Remove duplicate id-xy after image resizing
             # Sort by point id
+            # (on CPU or GPU)
             start = time()
             unique_idx = lexargunique(point_ids_, pix_x_, pix_y_, use_cuda=True)
             pix_x_ = pix_x_[unique_idx]
@@ -308,6 +314,7 @@ class MapImages(ImageTransform):
             t_unique_pixels += time() - start
 
             # Cast pixel coordinates to a dtype minimizing memory use
+            # (on CPU or GPU)
             start = time()
             pixels_ = torch.stack((pix_x_, pix_y_), dim=1).type(
                 image.pixel_dtype)
@@ -317,11 +324,12 @@ class MapImages(ImageTransform):
 
             # Gather per-image mappings in list structures, only to be
             # torch-concatenated once all images are processed
+            # (on CPU)
             start = time()
             image_ids.append(i_image)
-            point_ids.append(point_ids_)
-            features.append(features_)
-            pixels.append(pixels_)
+            point_ids.append(point_ids_.cpu())
+            features.append(features_.cpu())
+            pixels.append(pixels_.cpu())
             del pixels_, features_, point_ids_
             torch.cuda.synchronize()
             t_append += time() - start
@@ -339,7 +347,8 @@ class MapImages(ImageTransform):
         delattr(data, SphereSampling.KDTREE_KEY)
 
         # Raise error if no point-image-pixel mapping was found
-        image_ids = torch.LongTensor(image_ids).to(device)
+        # (on CPU)
+        image_ids = torch.LongTensor(image_ids)
         if image_ids.shape[0] == 0:
             raise ValueError(
                 "No mappings were found between the 3D points and any "
@@ -356,28 +365,32 @@ class MapImages(ImageTransform):
         # been seen, we remove it here.
         # NB: The reindexing here relies on the fact that `unique`
         #  values are expected to be returned sorted.
+        # (on CPU or GPU)
+        torch.cuda.synchronize()
         start = time()
-        seen_image_ids = lexunique(image_ids, use_cuda=True)
-        images = images[seen_image_ids]
+        seen_image_ids = lexunique(image_ids, use_cuda=self.use_cuda)
+        images = images[seen_image_ids.to(in_device)]
         image_ids = torch.bucketize(image_ids, seen_image_ids)
         del seen_image_ids
         if self.verbose:
             torch.cuda.synchronize()
             print(f"        t_index_image_data: {time() - start:0.3f}")
 
-        # Concatenate mappings data
+        # Concatenate mappings data (on CPU) and move them to required
+        # device (on CPU or GPU)
         start = time()
         image_ids = torch.repeat_interleave(
             image_ids,
-            torch.LongTensor([x.shape[0] for x in point_ids]).to(device))
-        point_ids = torch.cat(point_ids)
-        pixels = torch.cat(pixels)
-        # Cat on the features may blow up GPU memory, so do it on CPU
-        features = torch.cat([f.cpu() for f in features]).to(device)
+            torch.LongTensor([x.shape[0] for x in point_ids])).to(device)
+        point_ids = torch.cat(point_ids).to(device)
+        pixels = torch.cat(pixels).to(device)
+        features = torch.cat(features).to(device)
         if self.verbose:
-            torch.cuda.synchronize()
             print(f"        t_concat_dense_mappings_data: {time() - start:0.3f}")
 
+        # Compute the global ImageMapping
+        # (on CPU or GPU)
+        torch.cuda.synchronize()
         start = time()
         mappings = ImageMapping.from_dense(
             point_ids, image_ids, pixels, features,

torch_points3d/core/multimodal/image.py
@@ -1389,8 +1389,8 @@ class ImageMapping(CSRData):
         # NB: The pointers are marked by non-successive point-image ids.
         #     Watch out for overflow in case the point_ids and
         #     image_ids are too large and stored in 32 bits.
-        composite_ids = CompositeTensor(point_ids, image_ids,
-                                        device=point_ids.device)
+        composite_ids = CompositeTensor(
+            point_ids, image_ids, device=point_ids.device)
         image_pixel_mappings = CSRData(composite_ids.data, pixels, dense=True)
         del composite_ids
 
@@ -1400,8 +1400,8 @@ class ImageMapping(CSRData):
         image_ids = image_ids[image_pixel_mappings.pointers[1:] - 1]
         point_ids = point_ids[image_pixel_mappings.pointers[1:] - 1]
         if features is not None:
-            features = torch_scatter.segment_csr(features,
-                                                 image_pixel_mappings.pointers, reduce='mean')
+            features = torch_scatter.segment_csr(
+                features, image_pixel_mappings.pointers, reduce='mean')
 
         # Instantiate the main CSRData object
         # Compute point pointers in the image_ids array
@@ -1425,8 +1425,8 @@ class ImageMapping(CSRData):
 
         # Compress point_ids by taking the last value of each pointer
         point_ids = point_ids[mapping.pointers[1:] - 1]
-        mapping = mapping.insert_empty_groups(point_ids,
-                                              num_groups=num_points)
+        mapping = mapping.insert_empty_groups(
+            point_ids, num_groups=num_points)
 
         return mapping
 
@@ -1582,17 +1582,13 @@ class ImageMapping(CSRData):
         if isinstance(out.values[1], CSRBatch):
             sizes = out.values[1].__sizes__
             out.values[1] = CSRBatch(
-                ids,
-                torch.stack((pix_x, pix_y), dim=1).type(pix_dtype),
-                dense=True
-            )
+                ids, torch.stack((pix_x, pix_y), dim=1).type(pix_dtype),
+                dense=True)
             out.values[1].__sizes__ = sizes
         elif isinstance(out.values[1], CSRData):
             out.values[1] = CSRData(
-                ids,
-                torch.stack((pix_x, pix_y), dim=1).type(pix_dtype),
-                dense=True
-            )
+                ids, torch.stack((pix_x, pix_y), dim=1).type(pix_dtype),
+                dense=True)
         else:
             raise NotImplementedError(
                 "The atomic-level mappings must be either a CSRData or "
@@ -1635,8 +1631,8 @@ class ImageMapping(CSRData):
         # idx_gen so that the desired output can be computed with simple
         # indexation idx_gen[images]. This avoids using map() or
         # numpy.vectorize alternatives.
-        idx_gen = torch.full((idx.max() + 1,), -1, dtype=torch.int64,
-                             device=self.device)
+        idx_gen = torch.full(
+            (idx.max() + 1,), -1, dtype=torch.int64, device=self.device)
         idx_gen = idx_gen.scatter_(
             0, idx, torch.arange(idx.shape[0], device=self.device))
         out.images = idx_gen[out.images]
@@ -1697,8 +1693,8 @@ class ImageMapping(CSRData):
         # using map() or numpy.vectorize alternatives.
         img_idx = torch.unique(out.images)
         if img_idx.shape[0] < self.images.max() + 1:
-            idx_gen = torch.full((img_idx.max() + 1,), -1, dtype=torch.int64,
-                                 device=self.device)
+            idx_gen = torch.full(
+                (img_idx.max() + 1,), -1, dtype=torch.int64, device=self.device)
             idx_gen = idx_gen.scatter_(
                 0, img_idx, torch.arange(img_idx.shape[0], device=self.device))
             out.images = idx_gen[out.images]
@@ -1776,12 +1772,12 @@ class ImageMapping(CSRData):
             # Merge view-level mapping features
             if self.has_features:
                 # Compute composite point-image views ids
-                view_ids = CompositeTensor(point_ids, image_ids,
-                                           device=point_ids.device)
+                view_ids = CompositeTensor(
+                    point_ids, image_ids, device=point_ids.device)
                 view_ids = view_ids.data.squeeze()
                 # Average the features per view
-                features = torch_scatter.scatter_mean(self.features,
-                                                      view_ids, 0)
+                features = torch_scatter.scatter_mean(
+                    self.features, view_ids, 0)
                 # Prepare view indices for torch.gather
                 if features.dim() > 1:
                     view_ids = view_ids.view(-1, 1).repeat(1, features.shape[1])
@@ -1808,16 +1804,17 @@ class ImageMapping(CSRData):
             pixels = self.pixels
 
             # Remove duplicate pixel mappings and aggregate
-            idx_unique = lexargunique(point_ids, image_ids, pixels[:, 0],
-                                      pixels[:, 1])
+            idx_unique = lexargunique(
+                point_ids, image_ids, pixels[:, 0], pixels[:, 1])
             point_ids = point_ids[idx_unique]
             image_ids = image_ids[idx_unique]
             pixels = pixels[idx_unique]
             features = features[idx_unique] if features is not None else None
 
             # Convert to CSR format
-            out = ImageMapping.from_dense(point_ids, image_ids, pixels,
-                                          features, num_points=idx.max() + 1)
+            out = ImageMapping.from_dense(
+                point_ids, image_ids, pixels, features,
+                num_points=idx.max() + 1)
         else:
             raise ValueError(f"Unknown point selection mode '{mode}'.")
 
@@ -1890,8 +1887,8 @@ class ImageMapping(CSRData):
         pixels = pixels[cropped_in_idx]
 
         # Convert to CSR format
-        return ImageMapping.from_dense(point_ids, image_ids, pixels, features,
-                                       num_points=self.num_groups)
+        return ImageMapping.from_dense(
+            point_ids, image_ids, pixels, features, num_points=self.num_groups)
 
 
 class ImageMappingBatch(ImageMapping, CSRBatch):

torch_points3d/core/multimodal/visibility.py
@@ -185,6 +185,10 @@ def equirectangular_projection_cuda(
     w_pix = ((width - 1) * (1 - t / np.pi) / 2) % width
     h_pix = ((height - 1) * p / np.pi) % height
 
+    # Nan values may appear in extreme cases, set them to zero
+    w_pix[torch.where(w_pix.isnan())] = 0
+    h_pix[torch.where(h_pix.isnan())] = 0
+
     return w_pix, h_pix
 
 
@@ -1180,7 +1184,6 @@ class BiasuttiVisibility(VisibilityModel, ABC):
 
     def _visibility(self, *args, **kwargs):
         return visibility_biasutti(*args, **self.__dict__, **kwargs)
-
 
 # TODO: support other camera models than equirectangular image
 # TODO: support other depth map files formats. For now, only S3DIS format supported