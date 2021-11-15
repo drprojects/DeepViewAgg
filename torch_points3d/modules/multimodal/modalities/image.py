fixing scaling erros in image inference on Cityscapes model

torch_points3d/modules/multimodal/modalities/image.py
@@ -1201,7 +1201,7 @@ class CityscapesResNet18(nn.Module):
             conv1,
             nn.BatchNorm2d(128),
             nn.ReLU(inplace=True),
-            nn.MaxPool2d(kernel_size=3, stride=2, padding=1))
+            nn.MaxPool2d(kernel_size=3, stride=2, padding=0))
         self.layer1 = self._make_layer(block, 64, layers[0])
         self.layer2 = self._make_layer(block, 128, layers[1], stride=2)
         self.layer3 = self._make_layer(block, 256, layers[2], stride=2)