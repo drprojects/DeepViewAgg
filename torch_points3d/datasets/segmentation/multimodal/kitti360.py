b0ae97c061e698f534a8b3f6c1886e59595f74af
Damien <wl5719@engie.com>
2022-04-29 11:51:01 +0200

    editing kitti360 configs to avoid ambiguities in the way train_sample_res is defined

--- torch_points3d/datasets/segmentation/multimodal/kitti360.py
+++ torch_points3d/datasets/segmentation/multimodal/kitti360.py
@@ -640,8 +640,8 @@ class KITTI360DatasetMM(BaseDatasetMM):
         cls = MiniKITTI360CylinderMM if dataset_opt.get('mini', False) \
             else KITTI360CylinderMM
         radius = dataset_opt.get('radius')
-        train_sample_res = dataset_opt.get('train_sample_res')
-        eval_sample_res = dataset_opt.get('eval_sample_res')
+        train_sample_res = dataset_opt.get('train_sample_res', radius / 10)
+        eval_sample_res = dataset_opt.get('eval_sample_res', radius)
         image_r_max = dataset_opt.get('image_r_max')
         image_ratio = dataset_opt.get('image_ratio')
         image_size = tuple(dataset_opt.get('resolution_2d'))
